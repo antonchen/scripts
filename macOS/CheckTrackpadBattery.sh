8f0d90b44571dcc344cebbf8a9c6c708e40cde46
Anton Chen <contact@antonchen.com>
2018-09-04 12:07:45 +0800

    Update CheckTrackpadBattery.sh

--- macOS/CheckTrackpadBattery.sh
+++ macOS/CheckTrackpadBattery.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Create Date: 2017-05-26 17:03:49
-# Last Modified: 2017-05-26 17:31:52
+# Last Modified: 2018-09-04 12:04:26
 # Author: Anton Chen
 # Email: contact@antonchen.com
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
-system_profiler SPBluetoothDataType|grep -A 24 'Devices'|grep -q Trackpad
+system_profiler SPBluetoothDataType|sed -n '/Trackpad/,/Vendor ID/p'|grep -q 'Connected: Yes'
 if [ $? -ne 0 ]; then
-    echo "Trackpad 未连接"
-    exit 1
+    echo "Trackpad 正在充电或未连接"
+    exit
 fi
 
-batteryLevel=$(system_profiler SPBluetoothDataType|grep -A 24 'Devices'|grep Battery|awk -F"[:| |%]+" '{print $4}')
+batteryLevel=$(system_profiler SPBluetoothDataType|sed -n '/Trackpad/,/Vendor ID/p'|grep Battery|awk -F"[:| |%]+" '{print $4}')
 if [ $batteryLevel -lt 20 ]; then
     osascript -e "tell application \"System Events\" to display notification \"电量低，剩余 $batteryLevel%\" with title \"Magic Trackpad 2\" sound name \"Submarine\""
 fi
